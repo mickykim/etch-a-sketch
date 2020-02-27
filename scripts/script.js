3e2e95478637354827222f2f414d1b27a55e9727
Micky K <miqueaskimhan@gmail.com>
2020-02-27 17:55:05 -0600

    Added ability to resize grid through a prompt and improved visual clarity

--- scripts/script.js
+++ scripts/script.js
@@ -4,21 +4,19 @@ let height = 16;
 function createBoard(){
     const containerElement = document.querySelector('.container');
 /* Set the starting position of each item and append it to parent */
-    for(i = 0; i < width; i++){
-        for(j = 0; j < height; j++){
+    for(i = 0; i < height; i++){
+        for(j = 0; j < width; j++){
             const divElement = document.createElement('div');
-            divElement.style.gridColumn = i + 1;
-            divElement.style.gridRow = j + 1;
-            divElement.style.height = '100%';
-            divElement.style.width = '100%';
+            divElement.setAttribute('style', `grid-row: ${i + 1}; grid-column: ${j + 1}`);
             divElement.classList.add('uncolored');
             containerElement.appendChild(divElement);
         }
     }
-    /* Set the number of rows and columns for the grid */
+    /* Set the number of rows and columns for the grid container */
     containerElement.style.gridTemplateRows = 'repeat(height, 1fr)';
     containerElement.style.gridTemplateColumns = 'repeat(width, 1fr)';
     
+    /* Select newly created divs and add color functionality */
     const divArray = document.querySelectorAll('div.uncolored');
     divArray.forEach(currDiv => {
         currDiv.addEventListener('mouseover', (e) =>{
@@ -28,17 +26,27 @@ function createBoard(){
     });
 }
 
-function newDimensions(){
-
+/* Set new dimensions for board */
+function setDimensions(){
+    width = prompt('Please enter number of squares per side, 16');
+    height = width;
 }
 
+/* Delete previously created divs on container */
 function eraseBoard(){
-
+    const containerElement = document.querySelector('.container');
+    const divArray = document.querySelectorAll('div.uncolored, div.colored');
+    divArray.forEach(currDiv => {
+        containerElement.removeChild(currDiv);
+    });
 }
 
 const newPageButton = document.querySelector('#newPage');
 newPageButton.addEventListener('click', () =>{
     eraseBoard();
+    setDimensions();
     createBoard();
 });
+
+createBoard();
 
